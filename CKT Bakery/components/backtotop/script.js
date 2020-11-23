390a2d96d584b70bb3af0c91802c75835f75196a
ngtsnn <tusocnau@gmail.com>
2020-11-23 18:29:37 +0700

    new update for branch chi

--- CKT Bakery/components/backtotop/script.js
+++ CKT Bakery/components/backtotop/script.js
@@ -1,5 +1,5 @@
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
