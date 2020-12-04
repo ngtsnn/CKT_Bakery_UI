2f88e1f5fc73bfd94b77e06911ce5dc5035d7738
ngtsnn <tusocnau@gmail.com>
2020-12-04 14:05:20 +0700

    update drag n drop upload image - not really cool but still good

--- CKT Bakery/components/image-upload/script.js
+++ CKT Bakery/components/image-upload/script.js
@@ -1,1 +1,36 @@
-$(".file-upload").file_upload();
+const imgUploadInput = document.querySelector('#img-upload')
+const reviewBox = document.querySelector('#review-box');
+const reviewImg = reviewBox.querySelector('img');
+
+if (reviewImg.getAttribute('src')){
+    if (reviewBox.classList.contains('d-none')){
+        reviewBox.classList.remove('d-none');
+    }
+}
+else{
+    if (!reviewBox.classList.contains('d-none')){
+        reviewBox.classList.add('d-none');
+    }
+}
+
+imgUploadInput.addEventListener('change', () => {
+    const img = imgUploadInput.files[0];
+    if (img){
+        const imgReader = new FileReader();
+        imgReader.readAsDataURL(img);
+        imgReader.addEventListener('load', () => {
+            reviewImg.setAttribute('src', imgReader.result);
+            if (reviewBox.classList.contains('d-none')){
+                reviewBox.classList.remove('d-none');
+            }
+        })
+
+        
+    }
+    else{
+        if (!reviewBox.classList.contains('d-none')){
+            reviewBox.classList.add('d-none');
+        }
+    }
+    
+})
