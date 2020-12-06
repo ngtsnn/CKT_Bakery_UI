6625a8539ee3edcf065a716a538edca58762ec9b
ngtsnn <tusocnau@gmail.com>
2020-12-07 02:07:28 +0700

    may be this is the final code of Trung

--- CKT Bakery/components/side-list/script.js
+++ CKT Bakery/components/side-list/script.js
@@ -1,30 +1,39 @@
-const sideListItems = document.querySelectorAll(".side-list-item");
+const deskSideList = document.querySelector(".side-list");
+const mobileSideList = document.querySelector(".mobile-side-list");
+const deskSideListItems = deskSideList.querySelectorAll(".side-list-item");
+const mobileSideListItems = mobileSideList.querySelectorAll(".side-list-item");
 const sideTitle = document.querySelector("#side-title");
 
-sideListItems.forEach((item) => {
+console.log(deskSideList)
+console.log(mobileSideList)
+
+deskSideListItems.forEach((item, index) => {
   item.addEventListener("click", () => {
     let activeItems = document.querySelectorAll(".side-list-item.active");
     activeItems.forEach((activeItem) => {
       activeItem.classList.remove("active");
     });
     item.classList.add("active");
+    mobileSideListItems[index].classList.add("active")
     sideTitle.innerText = item.innerText;
   });
 });
 
-$(".owl-carousel").owlCarousel({
+mobileSideListItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    let activeItems = document.querySelectorAll(".side-list-item.active");
+    activeItems.forEach((activeItem) => {
+      activeItem.classList.remove("active");
+    });
+    item.classList.add("active");
+    deskSideListItems[index].classList.add("active")
+    sideTitle.innerText = item.innerText;
+  });
+});
+
+$("#m-side-list").owlCarousel({
   loop: false,
   margin: 10,
   nav: false,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    450: {
-      items: 2,
-    },
-    600: {
-      items: 3.4,
-    },
-  },
+  autowidth: true,
 });
