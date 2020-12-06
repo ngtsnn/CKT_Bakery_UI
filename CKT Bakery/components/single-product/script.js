56d559a7d723dc43ca56fb21794cb9ba7ce02b0d
trung <18521551@gm.uit.edu.vn>
2020-12-07 03:17:15 +0700

    fix something

--- CKT Bakery/components/single-product/script.js
+++ CKT Bakery/components/single-product/script.js
@@ -3,6 +3,8 @@ const counter = document.querySelector('.counter');
 const btnInc = document.querySelector('.btn-inc');
 const btnDesc = document.querySelector('.btn-desc');
 const patt1 = /\D/g;
+const wishListBtn0 = document.getElementById("id-add-wish-list");
+wishListBtn0.style.color = "rgb(0, 123, 255)";
 
 btnInc.addEventListener('click', () => {
     counter.value = Number(counter.value) + 1;
@@ -21,3 +23,15 @@ counter.addEventListener('keyup',()=>{
     
     counter.value = counter.value.replace(patt1,'');
 })
+
+
+function addWish() {
+    const wishListBtn = document.getElementById("id-add-wish-list");
+    console.log(wishListBtn.style.color);
+    if(wishListBtn.style.color == "rgb(255, 0, 129)"){
+        wishListBtn.style.color = "rgb(0, 123, 255)";
+    }
+    else
+        wishListBtn.style.color = "rgb(255, 0, 129)";
+
+}
