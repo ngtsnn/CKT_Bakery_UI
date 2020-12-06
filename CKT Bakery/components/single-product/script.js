25615808d61b3a4ffc9089d29df01e88e12ca9a5
HuynhNhatKhanh <khanhhuynh28082000@gmail.com>
2020-12-06 15:11:32 +0700

    repair single-product

--- CKT Bakery/components/single-product/script.js
+++ CKT Bakery/components/single-product/script.js
@@ -1,13 +1,23 @@
 
-        const counter = document.querySelector('.counter');
-        const btnInc = document.querySelector('.btn-inc');
-        const btnDesc = document.querySelector('.btn-desc');
+const counter = document.querySelector('.counter');
+const btnInc = document.querySelector('.btn-inc');
+const btnDesc = document.querySelector('.btn-desc');
+const patt1 = /\D/g;
 
-        btnInc.addEventListener('click', () => {
-            counter.innerHTML = Number(counter.innerHTML)+1;
-        });
-        btnDesc.addEventListener('click', () => {
-            if(counter.innerHTML>1){
-                counter.innerHTML = Number(counter.innerHTML)-1;
-            }
-        });
+btnInc.addEventListener('click', () => {
+    counter.value = Number(counter.value) + 1;
+    counter.focus();
+});
+btnDesc.addEventListener('click', () => {
+    if (counter.value > 1) {
+        counter.value = Number(counter.value) - 1;
+        counter.focus();
+    }
+});
+counter.addEventListener('keyup',()=>{
+    if(counter.value===''){
+        counter.value = 1;
+    }
+    
+    counter.value = counter.value.replace(patt1,'');
+})
