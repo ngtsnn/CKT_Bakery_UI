7336f4b24713f6a9f26f8ef026de226fe266e061
ngtsnn <tusocnau@gmail.com>
2020-12-08 21:46:28 +0700

    make setting and href all nav bar

--- CKT Bakery/components/pushback-login/script.js
+++ CKT Bakery/components/pushback-login/script.js
@@ -1,4 +1,4 @@
-if (!window.sessionStorage.getItem('signed')){
+if (window.sessionStorage.getItem('signed') != "true"){
     window.sessionStorage.setItem('page', window.location.href)
     window.location.href = './login.html'
 }
